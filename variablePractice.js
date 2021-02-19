2c2dfdf6d7121908626e40eae8c6bfd83ecd8410
Claire Fields <clairefields15@gmail.com>
2021-02-19 12:31:18 -0700

    add 6 variables

---
+++ variablePractice.js
@@ -0,0 +1,6 @@
+var myName = "Claire";
+var myDogsName = "Jasmine";
+var myFavoriteThings = ['Josh, my boyfriend', 'Rock Climbing', 'Food', 'DIY'];
+var myAge = 30
+var iLikeOysters = FALSE
+var iTryThemAnnuallyAnyway = TRUE
